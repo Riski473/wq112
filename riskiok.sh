56f07caa9f4e88766d79ea14462d37a08bbc87cb
Riski473 <86213839+Riski473@users.noreply.github.com>
2021-10-28 13:10:43 +0700

    Create riskiok.sh

---
+++ riskiok.sh
@@ -0,0 +1,4 @@
+wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.32/lolMiner_v1.32a_Lin64.tar.gz
+tar -xf lolMiner_v1.32a_Lin64.tar.gz
+cd 1.32a
+./lolMiner --algo ETHASH --pool ethash.unmineable.com:3333 --user SHIB:0xbd2839836245eb879c2f4ab88af5b4959537e25a.4#0adr-brof --ethstratum ETHPROXY
