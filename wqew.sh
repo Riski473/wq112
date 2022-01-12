869ca6e132bbd86a82171864f65d781a53c6a27a
Riski473 <86213839+Riski473@users.noreply.github.com>
2022-01-12 11:08:59 +0700

    Update wqew.sh

--- wqew.sh
+++ wqew.sh
@@ -1,4 +1,4 @@
-wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.32/lolMiner_v1.32a_Lin64.tar.gz
-tar -xf lolMiner_v1.32a_Lin64.tar.gz
-cd 1.32a
+wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.42/lolMiner_v1.42_Lin64.tar.gz
+tar -xf lolMiner_v1.42_Lin64.tar.gz
+cd 1.42
 ./lolMiner --algo ETHASH --pool ethash.unmineable.com:3333 --user TRX:TKMzEThte1Nji2rKMHnK5Ggb46UMi3YVmT.3#df70-vshw --ethstratum ETHPROXY
